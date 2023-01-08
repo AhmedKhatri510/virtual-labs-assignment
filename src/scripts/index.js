dcc359cff341608ca8c856527d5cca794dc833c1
AhmedKhatri510 <ahmedkhatri510@gmail.com>
2023-01-08 20:19:06 +0530

    added table ui

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,43 @@
-import arr from "./constants/tableData";
+// import arr from "../constants/tableData";
+import arr from "./constants/tableData.js";
+
 console.log(arr);
+
+window.onload = function () {
+  const tbody = document.getElementsByClassName("table__container--body")[0];
+  console.log(tbody);
+  let row, cell;
+
+  for (let i = 0; i < arr.length; i++) {
+    row = tbody.insertRow();
+
+    for (let [key, value] of Object.entries(arr[i])) {
+      if (key === "clientType") continue;
+      cell = row.insertCell();
+
+      if (String(value).toLowerCase().includes("draft")) {
+        cell.id = "draft";
+      }
+      if (String(value).toLowerCase().includes("paid")) {
+        cell.id = "paid";
+      }
+      if (String(value).toLowerCase().includes("partial payment")) {
+        cell.id = "partial-payment";
+      }
+
+      if (
+        key.toLowerCase().includes("total") ||
+        key.toLowerCase().includes("balance")
+      ) {
+        value = `$${value}`;
+      }
+      cell.innerHTML = value;
+
+      console.log(cell);
+    }
+  }
+
+  // row = tbody.insertRow();
+  // cell = row.insertCell();
+  // cell.innerHTML = "The first cell";
+};
